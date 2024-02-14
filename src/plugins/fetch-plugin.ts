abed6b5aa543707bf4c9b9438550cf32f3a0da91
theolodocoder <theolodocoder@gmail.com>
2024-02-14 11:00:20 +0100

    fix:esbuild to handle css

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -12,26 +12,28 @@ export const fetchPlugin = (inputCode:string) => {
     name: "fetch-plugin",
     setup(build:esbuild.PluginBuild){
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    build.onLoad({filter:/(^index\.js$)/},() => {
+      return {
+        loader: "jsx",
+        contents: inputCode,
+      };
+    })
     build.onLoad({ filter: /.*/ }, async (args:any) => {
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: inputCode,
-          };
-        }
         // check if the file to load already exist in our cache
-        const cachedFile = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path
-        );
-        // if so return it
-        if (cachedFile) {
-          return cachedFile;
-        }
+        // const cachedFile = await fileCache.getItem<esbuild.OnLoadResult>(
+        //   args.path
+        // );
+        // // if so return it
+        // if (cachedFile) {
+        //   return cachedFile;
+        // }
         // if we dont have it the we can make the request
+        const fileType = args.path.match(/.css$/) ? 'css':'jsx'
         const { data, request } = await axios.get(args.path);
-        const contents = args.path.match(/.css$/) ? `
+        const escaped = data.replace(/\n/g,''.replace(/"/g, '\\"')).replace(/'/g,"\\'");
+        const contents = fileType == 'css'? `
           const style = document.createElement("style")
-          style.innerText = 'body:{background:red}'
+          style.innerText = '${escaped}'
           document.head.appendChild(style)
         `:data
         const result: esbuild.OnLoadResult = {
