dcd05d88c9f252ec0b0ffe042621b58165b072bd
theolodocoder <theolodocoder@gmail.com>
2024-02-17 22:36:51 +0100

    feat:added prettier for code formatting

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,29 +1,65 @@
-import { Editor } from "@monaco-editor/react"
-import { editor } from "monaco-editor"
-import React from "react"
+import { useRef } from "react";
+import { Editor } from "@monaco-editor/react";
+import { editor } from "monaco-editor";
+import * as prettier from "prettier";
+import parser from "prettier/parser-babel";
 
-interface CodeEditorProps{
-  initialValue :string
-  onChange:(input:string) => void
+interface CodeEditorProps {
+  initialValue: string;
+  onChange: (input: string) => void;
 }
-const CodeEditor:React.FC<CodeEditorProps> = ({initialValue,onChange}) => {
-  const handleOnMount = (editor:editor.IStandaloneCodeEditor,_) => {
-    editor.onDidChangeModelContent(()=> {
-      onChange(editor.getValue())
-    })
-    editor.getModel()?.updateOptions({tabSize:2})
-  }
-  return <Editor height={"90vh"} language="javascript" theme="vs-dark" value={initialValue} onMount={handleOnMount} options={{
-    wordWrap:"on",
-    minimap:{enabled:false},
-    showUnused:false,
-    folding:false,
-    fontLigatures:true,
-    fontSize:16,
-    scrollBeyondLastLine:false,
-    lineNumbersMinChars:3,
-    automaticLayout:true
-  }} />
-}
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const editorRef = useRef<any>(null);
 
-export default CodeEditor
+  const handleOnMount = (editor: editor.IStandaloneCodeEditor, _) => {
+    // on mount set the editorRef
+    editorRef.current = editor;
+    editor.onDidChangeModelContent(() => {
+      onChange(editor.getValue());
+    });
+    editor.getModel()?.updateOptions({ tabSize: 2 });
+  };
+
+  const handleFormat = async () => {
+    // get the current value from the editor
+    const unformattedCode = editorRef.current.getModel().getValue();
+
+    // format the code using prettier
+    const formattedCode =prettier.format(unformattedCode, {
+      parser: "babel",
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    });
+
+    // set the editors code back to the formatted code
+    editorRef.current.setValue(formattedCode)
+  };
+
+  return (
+    <>
+      <button onClick={handleFormat}>Format</button>
+      <Editor
+        height={"90vh"}
+        language="javascript"
+        theme="vs-dark"
+        value={initialValue}
+        onMount={handleOnMount}
+        options={{
+          wordWrap: "on",
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          fontLigatures: true,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          lineNumbersMinChars: 3,
+          automaticLayout: true,
+        }}
+      />
+    </>
+  );
+};
+
+export default CodeEditor;
